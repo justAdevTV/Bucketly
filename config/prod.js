fb55dcc9668bf5bd9429518bb6db97ac42a32ac1
Justin <justpunz@live.com>
2018-02-22 19:19:16 -0800

    fixed type in prod vars

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,5 @@ module.exports = {
     facebookAppID: process.env.FACEBOOK_CLIENT_ID,
     facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: Process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY
 };

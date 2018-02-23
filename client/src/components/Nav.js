3f425934c3cde6f49b0a0c2fcce916db06dc9598
Justin <justpunz@live.com>
2018-02-22 20:05:46 -0800

    Added Sign Up Button to Nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -9,14 +9,12 @@ class Nav extends Component {
                         <a href="#!" class="brand-logo">Bucketly</a>
                         <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
                         <ul className="right hide-on-med-and-down">
-                            <li><a href="sass.html">Sass</a></li>
-                            <li><a href="badges.html">Components</a></li>
-                            <li><a href="collapsible.html">Javascript</a></li>
+                            <li><a href="">Log In</a></li>
+                            <li><a class="waves-effect waves-light btn">Sign Up</a></li>
                         </ul>
                         <ul className="side-nav" id="mobile-demo">
-                            <li><a href="sass.html">Sass</a></li>
-                            <li><a href="badges.html">Components</a></li>
-                            <li><a href="collapsible.html">Javascript</a></li>
+                            <li><a href="">Log In</a></li>
+                            <li><a class="waves-effect waves-light btn">Sign Up</a></li>
                         </ul>
                     </div>
                 </div>    
